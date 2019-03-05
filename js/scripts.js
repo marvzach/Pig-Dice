c12c6ed723d6de37e5d343e80d8a52f215f5e045
Marvin Zachary <acerzach@gmail.com>
2019-03-05 14:05:20 +0300

    Added a click function to get the number rolled and show message if you rolled one

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,18 @@ $(function() {
     var player2 = new Player(player2Name, 0, 0)
     allPlayers.push(player1);
     allPlayers.push(player2);
+    $(".player1-name").text(player1.playerName);
+    $(".Player1Overal").html("<span class='Player1Overal'>" + player1.totalScore + "</span>");
+
+    $("button#onedice-roll").click(function(event) {
+      event.preventDefault();
+      var player1RolledNumber = player1.roll();
+      if (player1RolledNumber === 1) {
+        $(".player1").hide();
+        $(".player2").show();
+        $(".messagePlayer1").show();
+      }
+      $(".oneDiceroll").text(player1RolledNumber);
+      $(".oneCurrentscore").text(player1.turnScore);
+      $(".messagePlayer2").hide();
+    });
